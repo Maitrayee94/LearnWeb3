d8650daa1d211ca85b203a0b5d1657c89c1cec79
MD <117962426+Maitrayee94@users.noreply.github.com>
2023-01-12 20:30:28 +0530

    Add files via upload

---
+++ DAO-tutorial/solidity/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x05aB6cbAc53F1651986bAe020AAc347808E1162f";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
